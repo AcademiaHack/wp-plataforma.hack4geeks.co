653e96b09013498ba909240a9372026e80712cf1
Glister <jfuentes@stcsolutions.com.ve>
2014-08-06 11:12:28 -0430

    404 meanwhile

--- wp-content/themes/HackRoots/404.php
+++ wp-content/themes/HackRoots/404.php
@@ -1,6 +1,19 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<div class="col-xs-12 col-sm-6 pull-right">
+  <div class="img-container">
+    <div class="error-text">
+      <h1>
+        <span>ERROR</span><br>
+        404
+      </h1>
+      <p>
+      	<?php _e('Page not found','roots'); ?>
+      </p>
+    </div>
+  </div>
+</div>
 
-<div class="alert alert-warning">
+
+<!-- <div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
 </div>
 
@@ -8,6 +21,4 @@
 <ul>
   <li><?php _e('a mistyped address', 'roots'); ?></li>
   <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
+</ul> -->
