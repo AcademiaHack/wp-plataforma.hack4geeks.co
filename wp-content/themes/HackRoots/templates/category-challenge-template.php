9073439e52fb8436aea4f9a3e313194e8a36a8bd
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2014-08-18 17:21:56 -0430

    inicio de vista de actividades detalladas

--- wp-content/themes/HackRoots/templates/category-challenge-template.php
+++ wp-content/themes/HackRoots/templates/category-challenge-template.php
@@ -1,3 +1,41 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<h3>Esta es la pagina para los Retos</h3>
+<header class="reto-header">
+	<div class="badges">
+		<div class="badges">
+			<div class="badge-container off">
+				<a href="#">
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_2.png" alt="">
+				</a>
+			</div>
+			<div class="badge-container off">
+				<a href="#">
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_3.png" alt="">
+				</a>
+			</div>
+			<div class="badge-container">
+				<a href="#">
+					<img class="img-responsive badge-img on" src="wp-content/themes/HackRoots/assets/img/1_1.png" alt="">
+				</a>
+			</div>
+			<div class="badge-container off">
+				<a href="#">
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_4.png" alt="">
+				</a>
+			</div>
+			<div class="badge-container off">
+				<a href="#">
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_5.png" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+</header>
+
+<div class="reto-background">
+	<div class="container">
+		<h1 class="text-center">
+			Como buscar, HTML
+		</h1>
+	</div>
+</div>
