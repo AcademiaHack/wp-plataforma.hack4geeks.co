ae135b9378816608ba13c18ea3cdbe6d9cf0677d
Leonel Maldonado <lmaldonado@4geeks.co>
2014-08-20 17:38:40 -0430

    plugin field extra, pagina de categoria

--- wp-content/themes/HackRoots/templates/category-challenge-template.php
+++ wp-content/themes/HackRoots/templates/category-challenge-template.php
@@ -1,32 +1,71 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/page', 'header'); 
+global $wpdb;
+ 
+$category =get_queried_object(); 
+
+$nimagen = get_tax_meta($category->term_id,'text_cat_id'); 
+$nanterior= $nimagen-1;
+$naanterior = $nanterior-2;
+$nposterior = $nimagen+1;
+$npposterior = $nimagen+2;
+$lanterior= $nimagen-1;
+$laanterior = $nanterior-2;
+$lposterior = $nanterior+1;
+$lpposterior = $nanterior+2;
+if ($nimagen-1<=0)
+{
+	$nanterior = 0;
+	$naanterior = 0;
+}
+else if ($nimagen-2<=0)
+{
+	$naanterior = 0;
+}
+else if ($nimagen-1<=0)
+{
+	$nanterior = 0;
+	$naanterior = 0;
+}
+else if ($nimagen+1>=56)
+{
+	$nposterior = 0;
+	$npposterior = 0;
+}
+else if ($nimagen+2>=56)
+{ 
+	$npposterior = 0;
+}
+$posts = query_posts( 'cat='.$category->term_id	 ); 
+
+?>
 
 <header class="reto-header">
 	<div class="badges">
 		<div class="badges">
 			<div class="badge-container off">
-				<a href="#">
-					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_2.png" alt="">
-				</a>
+				 
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/cat/<?php echo $naanterior?>.png" alt="">
+			 
 			</div>
 			<div class="badge-container off">
-				<a href="#">
-					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_3.png" alt="">
-				</a>
+				 
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/cat/<?php echo $nanterior?>.png" alt="">
+				 
 			</div>
 			<div class="badge-container">
-				<a href="#">
-					<img class="img-responsive badge-img on" src="wp-content/themes/HackRoots/assets/img/1_1.png" alt="">
-				</a>
+			 
+					<img class="img-responsive badge-img on" src="wp-content/themes/HackRoots/assets/img/cat/<?php echo $nimagen?>.png" alt="">
+				 
 			</div>
 			<div class="badge-container off">
-				<a href="#">
-					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_4.png" alt="">
-				</a>
+				 
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/cat/<?php echo $nposterior?>.png" alt="">
+				 
 			</div>
 			<div class="badge-container off">
-				<a href="#">
-					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/1_5.png" alt="">
-				</a>
+				 
+					<img class="img-responsive badge-img off" src="wp-content/themes/HackRoots/assets/img/cat/<?php echo $npposterior?>.png" alt="">
+				 
 			</div>
 		</div>
 	</div>
@@ -35,7 +74,7 @@
 <div class="reto-background">
 	<div class="container">
 		<h1 class="text-center">
-			Como buscar, HTML
+			<?php echo $category->name ?>
 		</h1>
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2">
@@ -45,22 +84,20 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-6">
-				<img class="img-responsive" src="wp-content/themes/HackRoots/assets/img/challenge-accepted.jpg" alt="">
-				<h2 class="text-center">Hacer algo increiblemente increible</h2>
-				<a href="http://localhost/Hack/?p=34" class="btn btn-primary btn-block btn-flat btn-large">
+		<?php  
+			foreach ($posts as $post){
+					 
+		?>
+			<div class="col-sm-6"> 
+				<img class="img-responsive" src="<?php echo wp_get_attachment_url(get_post_thumbnail_id( $post->ID)) ?>" alt="">
+				<h2 class="text-center"><?php echo $post->post_title ?></h2>
+				<a href="<?php echo $post->guid ?>" class="btn btn-primary btn-block btn-flat btn-large">
 					<img src="wp-content/themes/HackRoots/assets/img/list-hexagon.png" alt="">
-					¡Hazlo!
+					<?php _e('¡Hazlo!','roots'); ?>
 				</a>
 			</div>
-			<div class="col-sm-6">
-				<img class="img-responsive" src="wp-content/themes/HackRoots/assets/img/challenge-accepted.jpg" alt="">
-				<h2 class="text-center">Hacer algo increiblemente increible</h2>
-				<a href="http://localhost/Hack/?p=34" class="btn btn-primary btn-block btn-flat btn-large">
-					<img src="wp-content/themes/HackRoots/assets/img/list-hexagon.png" alt="">
-					¡Hazlo!
-				</a>
-			</div>
+			 
+		<?php }	?>
 		</div>
 	</div>
 </div>
