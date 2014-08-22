a82d63e68ab750224c4e7c523bfc4076f0ae0476
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2014-08-22 16:34:36 -0430

    vista de proximamente

--- wp-content/themes/HackRoots/template-prox.php
+++ wp-content/themes/HackRoots/template-prox.php
@@ -7,7 +7,14 @@ Template Name: Prox Template
 <?php while (have_posts()) : the_post(); ?>
    
   <div class="container">
-    
-  </div> 	
+      <div class="main_info">
+        <div class="text-center">
+          <a href="#">
+            <img src="<?php echo get_theme_root_uri(); ?>/HackRoots/assets/img/logo-soon.png">
+          </a>
+        </div>
+        <h1 class="white-box-shadow text-center">Próximamente</h1>
+      </div>
+    </div>	
 
 <?php endwhile; ?>
