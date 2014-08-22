71b22c15a9e76f5d90ce21bf841e82faa69ba110
Leonel Maldonado <lmaldonado@4geeks.co>
2014-08-22 15:29:44 -0430

    Agregar comentarios pagina noticias, nombre del usuario en el header

--- wp-content/themes/HackRoots/single.php
+++ wp-content/themes/HackRoots/single.php
@@ -1,7 +1,11 @@
+ 
 <?php
-	if(get_the_category()[0]->cat_ID == 2){
+	$noticias_id = get_cat_ID('noticias');
+	 
+	if(get_the_category()[0]->cat_ID == $noticias_id){
+	 
 		get_template_part('templates/content', 'single-news'); 
-	}else{
+	}else{ 
 		get_template_part('templates/content', 'single-challenge'); 
 	}
 ?>
