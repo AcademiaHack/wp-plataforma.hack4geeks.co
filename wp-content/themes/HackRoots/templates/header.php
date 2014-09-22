85eba4ffbc22faa537457a78d60fdb0f587e3f9b
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2014-09-22 16:26:14 -0430

    responsive general

--- wp-content/themes/HackRoots/templates/header.php
+++ wp-content/themes/HackRoots/templates/header.php
@@ -1,9 +1,9 @@
 <?php
     // Get the ID of a given category
-    $category_id = get_cat_ID( 'noticias' );
+$category_id = get_cat_ID( 'noticias' );
 
     // Get the URL of this category
-    $category_link = get_category_link( $category_id );
+$category_link = get_category_link( $category_id );
 ?>
 <header class="navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
@@ -14,35 +14,35 @@
     </div>
     <nav id="categorias" class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
       <?php
-        if(is_user_logged_in()){
-          if (has_nav_menu('header_menu')){
-            wp_nav_menu(array('theme_location' => 'header_menu', 
-                              'menu_class' => 'nav navbar-nav',
-                              'after' => '<div class="caret_header"><span></span></div>',
-                              'walker' => new Roots_Nav_Walker));
-          }
+      if(is_user_logged_in()){
+        if (has_nav_menu('header_menu')){
+          wp_nav_menu(array('theme_location' => 'header_menu', 
+            'menu_class' => 'nav navbar-nav',
+            'after' => '<div class="caret_header"><span></span></div>',
+            'walker' => new Roots_Nav_Walker));
+        }
 
-          if(is_page_template('template-profile.php')){
-            if (has_nav_menu('header_profile')){
-              wp_nav_menu(array('theme_location' => 'header_profile',
-                                'menu_class' => 'nav navbar-nav navbar-right',
-                                'walker' => new Roots_Nav_Walker));
-            }
-          }else{
-            if (has_nav_menu('header_logged')){
-              wp_nav_menu(array('theme_location' => 'header_logged',
-                                'menu_class' => 'nav navbar-nav navbar-right',
-                                'walker' => new Roots_Nav_Walker));
-            }
-          }          
-        }else{
-          if (has_nav_menu('header_not_logged')){
-            wp_nav_menu(array('theme_location' => 'header_not_logged',
-                              'menu_class' => 'nav navbar-nav navbar-right',
-                              'after' => '<div class="caret_header"><span></span></div>',
-                              'walker' => new Roots_Nav_Walker));
+        if(is_page_template('template-profile.php')){
+          if (has_nav_menu('header_profile')){
+            wp_nav_menu(array('theme_location' => 'header_profile',
+              'menu_class' => 'nav navbar-nav navbar-right',
+              'walker' => new Roots_Nav_Walker));
           }
-        }       
+        }else{
+          if (has_nav_menu('header_logged')){
+            wp_nav_menu(array('theme_location' => 'header_logged',
+              'menu_class' => 'nav navbar-nav navbar-right',
+              'walker' => new Roots_Nav_Walker));
+          }
+        }          
+      }else{
+        if (has_nav_menu('header_not_logged')){
+          wp_nav_menu(array('theme_location' => 'header_not_logged',
+            'menu_class' => 'nav navbar-nav navbar-right',
+            'after' => '<div class="caret_header"><span></span></div>',
+            'walker' => new Roots_Nav_Walker));
+        }
+      }       
       ?>
     </nav>
     <div class="caret_header_2"></div>
